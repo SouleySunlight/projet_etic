599e98a20d6a481bbdf992f7875ba49b89fbbe9d
SouleySunlight <souleyman.benahdj@gmail.com>
2020-05-23 17:30:56 +0200

    nouvel affichage

--- test.js
+++ test.js
@@ -1,72 +1,51 @@
 var fs = require('fs');
 var chemin = '../neo4j-community-4.0.4/import/all.csv'
 var noeud = "{\"nodes\" : [\n";
-var rel = "{\"edges\" : [\n";
+var rel = "\"edges\" : [\n";
 
 fs.readFile(chemin, 'utf8', function (err, data) {
     var dataArray = data.split(/\r?\n/);
-    for (let i = 1; i < dataArray.length - 1; i++) {
+    for (let i = 0; i < dataArray.length - 1; i++) {
         var donnee = dataArray[i].split(',');
+        console.log(donnee);
         if (donnee[1] == '":TRANSITION"') {
-            if(noeud!="{\"nodes\" : [\n"){
-                noeud=noeud+","
+            if (noeud != "{\"nodes\" : [\n") {
+                noeud = noeud + ","
             }
             let s = donnee[3] + " " + donnee[4];
             s.replace(",", " ")
-            noeud = noeud + "{id:" + donnee[2] + "," +
-                " label:\"TRANSITION\"," +
-                "nom:" + s + "} \n"
+            noeud = noeud + "{\"id\":" + donnee[0] + "," +
+                " \"label\":\"TRANSITION\"," +
+                "\"nom\":" + s + "} \n"
         } else if (donnee[1] == '":ENJEU"') {
-            if(noeud!="{\"nodes\" : [\n"){
-                noeud=noeud+","
+            if (noeud != "{\"nodes\" : [\n") {
+                noeud = noeud + ","
             }
             let s = donnee[3] + " " + donnee[4];
-             let x=s.replace(',', ' ');
-             console.log(x);
-            noeud = noeud + "{id:" + donnee[2] + "," +
-                " label:\"ENJEU\"," +
-                "nom:" + x + "}\n"
+            let x = s.replace(',', ' ');
+            noeud = noeud + "{\"id\":" + donnee[0] + "," +
+                " \"label\":\"ENJEU\"," +
+                "\"nom\":" + x + ",\"color\":{\"background\":\"green\", \"highlight\":\"blue\"}}\n"
         } else {
-            if(rel!="{\"edges\" : [\n"){
-                rel=rel+","
+            if (rel != "\"edges\" : [\n") {
+                rel = rel + ","
             }
-            rel = rel + "{from:" + donnee[4] + "," +
-                " to:" + donnee[5] + "," +
-                "label:" + donnee[7]  +
+            rel = rel + "{\"from\":" + donnee[4] + "," +
+                " \"to\":" + donnee[5] + "," +
+                "\"label\":" + donnee[7] +
                 "}\n"
         }
     }
-    function afficher(a) {
-        a="function ok";
-    }
+    data = noeud + "],\n" + rel + "]}";
 
-    noeud= noeud + "] }";
-    rel=rel + "]}";
-
-    console.log(noeud);
-    console.log(rel);
-
-  /*  fs.writeFile('Uploads/noeud.json', noeud, function (err, data) {
+    fs.writeFile('views/data.json', data, function (err, data) {
         if (err) {
             return console.log(err)
         }
-
     });
-    fs.writeFile('Uploads/relation.json', rel, function (err, data) {
-        if (err) {
-            return console.log(err)
-        }
 
-    });*/
+
+});
 
 
 
-
-})
-
-/*let rawdata = fs.readFileSync('Uploads/noeud.json');
-let student = JSON.parse(rawdata);
-console.log(student);
-
-
-*/
